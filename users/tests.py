6758e30bd3fc187de4f018fe83f08a08d790d294
Yi wonchae <wonchae1019@gmail.com>
2022-11-15 10:16:40 +0900

    timeattack

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class UserRegistrationAPIViewTestCase(APITestCase):
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username" : "testuser",
+            "fullname" : "tester",
+            "email": "test@testuser.com",
+            "password":"password",
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.data["message"], "가입 완료!!")
--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     # user/
-    path('', views.UserView.as_view()),
+    path('', views.UserView.as_view(), name='user_data'),
+    # path('', views.UserRegistrationAPIViewTestCase.as_view(), name="user_register"),
     path('api/token/', views.SpartaTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
  
